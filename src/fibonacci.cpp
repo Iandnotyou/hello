0c213962e877cef1de0d277ef905a8fc45136499
Nat Brubaker <bs@nap.mynet>
2014-05-01 15:47:18 +0200

    .

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -12,9 +12,9 @@
 
 using namespace std;
 
-/*
+
 // rekursiv
-template <class T>
+template <typename T>
 T rek_fibonacci(T n) {
 	switch (static_cast<int>(n)) {
 	case 0: return n;
@@ -22,20 +22,20 @@ T rek_fibonacci(T n) {
 	default: return rek_fibonacci(n-1)+rek_fibonacci(n-2);
 	}
 }
-*/
+
 
 
 // iterativ
-template <class T>
+template <typename T>
 T fibonacci(T n) {
 	T current, pre = 1, prepre = 0;
 
 	if (n == 0) return 0;
 	if (n == 1) return 1;
     for (T i = 1;i < n; i++) {
-    	 if ((pre + prepre) > std::numeric_limits<T>::max())
-    		 throw overflow_error("overflow error");
 	     current = pre + prepre;
+    	 if (current != pre + prepre)
+    		 throw 999; // overflow_error("overflow error");
 	     prepre  = pre;
 	     pre     = current;
 
@@ -45,23 +45,43 @@ T fibonacci(T n) {
 
 
 int main() {
+
 	cout << "!!!Hello Fibonacci!!!" << endl;
-	for (unsigned char i=0;i<14;i++)
-		cout << (int)i << " " << (unsigned int)fibonacci(i) << endl;
+
+	for (unsigned char i=0;i<99;i++)
+		try {
+		  cout << (int)i << " " << (unsigned int)fibonacci(i) << endl;
+		} catch (const int &e) {
+	        std::cerr << "overflow at i = " << (unsigned int)i << endl;
+	      break;
+		}
 	cout << endl;
-	for (unsigned short i=0;i<24;i++)
-		cout << i << " " << (unsigned short)fibonacci(i) << endl;
+
+	for (unsigned int i=0;i<51;i++)
+		try {
+		  cout << (unsigned int)i << " " << (unsigned int)fibonacci(i) << endl;
+		} catch (const std::exception &e) {
+	        std::cerr << "overflow at i = " << (unsigned int)i << endl;
+	      break;
+		}
 	cout << endl;
-	for (unsigned int i=0;i<59;i++)
-		cout << i << " " << (unsigned int)fibonacci(i)      << endl;
+
+	for (unsigned long i=0;i<50;i++)
+		try {
+		  cout << (unsigned long)i << " " << (unsigned long)fibonacci(i) << endl;
+		} catch (const std::exception &e) {
+	      std::cerr << "overflow at " << i << endl;
+		}
 	cout << endl;
-	for (unsigned long i=0;i<68;i++)
-		cout << (unsigned long)fibonacci(i)      << endl;
+
+
+	for (unsigned long long i=0;i<97;i++)
+		try {
+		  cout << (unsigned long long)i << " " << (unsigned long long)fibonacci(i) << endl;
+		} catch (const std::exception &e) {
+	      std::cerr << "overflow at " << i << endl;
+		}
 	cout << endl;
-	for (unsigned long long i=0;i<99;i++)
-		cout << (unsigned long long)fibonacci(i) << endl;
-	cout << endl;
-	for (double i=0;i<99;i++)
-		cout << (double)fibonacci(i) << endl;
+
 	return 0;
 }
